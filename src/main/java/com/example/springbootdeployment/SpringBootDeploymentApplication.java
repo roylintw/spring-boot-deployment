68a458b6ffd60cd1027f41c0d6fc844f19e45c93
Roy Lin 林孟憲 <bizroylin@gmail.com>
2024-05-07 19:19:16 +0800

    Create Project

---
+++ src/main/java/com/example/springbootdeployment/SpringBootDeploymentApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDeploymentApplication.class, args);
+	}
+
+}
