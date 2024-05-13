56e3b148fb46c2d4403c154ea6a674d071475c77
roylin1995 <bizroylin@gmail.com>
2024-05-13 16:02:52 +0800

    HelloController 紀錄登入資訊

--- src/main/java/com/example/springbootdeployment/HelloController.java
+++ src/main/java/com/example/springbootdeployment/HelloController.java
@@ -1,11 +1,15 @@
 package com.example.springbootdeployment;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    public static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/")
     public String landingPage() {
@@ -15,6 +19,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(Authentication authentication) {
         String userName = authentication.getName();
+        logger.info("使用者 " + userName + " 登入中...");
         return "您好! " + userName + " 先生/女士";
     }
 
