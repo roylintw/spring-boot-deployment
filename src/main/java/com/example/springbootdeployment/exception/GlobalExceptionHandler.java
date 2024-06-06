243f202509a4414e910fe77ee18cabdfe7bbb25f
roylin1995 <bizroylin@gmail.com>
2024-06-06 22:29:08 +0800

    增加 MethodNotAllowedException log

--- src/main/java/com/example/springbootdeployment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/springbootdeployment/exception/GlobalExceptionHandler.java
@@ -1,13 +1,18 @@
 package com.example.springbootdeployment.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.MethodNotAllowedException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final static Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<String> handleBadRequestException(BadRequestException ex, WebRequest request) {
@@ -17,6 +22,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InternalServerErrorException.class)
     public ResponseEntity<String> handleInternalServerErrorException(InternalServerErrorException ex, WebRequest request) {
         return new ResponseEntity<>("Internal Server Error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodNotAllowedException.class)
+    public final ResponseEntity<Object> handleMethodNotAllowedException(MethodNotAllowedException ex, WebRequest request) {
+        String errorMessage = "Method Not Allowed: " + ex.getMessage();
+        log.error("Method Not Allowed Exception: ", ex);
+        return new ResponseEntity<>(errorMessage, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     // 其他異常處理方法...
